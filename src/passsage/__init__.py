47dd50d26ad50b17aefe184331355101e6a8299d
bra-fsn <nagy.attila@gmail.com>
2026-02-10 23:32:40 +0100

    bump

--- src/passsage/__init__.py
+++ src/passsage/__init__.py
@@ -9,7 +9,7 @@ requires mitmproxy's hooks to be registered first, which only happens when
 mitmproxy loads the proxy script.
 """
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 from passsage.policy import (
     AlwaysUpstream,
