6b08cdc3a65afac6696ddd4824c969a72fcffabc
bra-fsn <nagy.attila@gmail.com>
2026-02-11 09:57:29 +0100

    actually we can leave this as is, we don't win anything by using norefresh

--- src/passsage/default_policies.py
+++ src/passsage/default_policies.py
@@ -32,10 +32,9 @@ def default_rules() -> list["Rule"]:
         SuffixRule("/Packages.gz", StaleIfError),
         SuffixRule("/Packages.xz", StaleIfError),
         SuffixRule("/InRelease", StaleIfError),
-        RegexRule(r".*by-hash/[A-Z0-9]+/[a-f0-9]+$", NoRefresh),
-        # Python hosted packages (immutable content-addressed archives)
-        HostContainsRule("files.pythonhosted.org", NoRefresh),
-        # Python package index (mutable, new versions appear)
+        RegexRule(r".*by-hash/[A-Z0-9]+/[a-f0-9]+$", StaleIfError),
+        # Python hosted packages
+        HostContainsRule("files.pythonhosted.org", StaleIfError),
         PathContainsRule("pypi.org/simple", StaleIfError),
         # cloud metadata endpoints
         HostPrefixRule("169.254.169.", NoCache),
